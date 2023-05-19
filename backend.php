a3fe2d42b6432058a8660748b11afbf1db62df3a
nagachen <birdann99@gmail.com>
2023-05-19 22:35:21 +0800

    在編輯和刪除按鈕上加上資料id

--- backend.php
+++ backend.php
@@ -1,23 +1,59 @@
+<?php include_once "db.php" ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>管理後台</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
-<header>
-    <a href="index.php">首頁</a>
-    <a href="login.php">登出</a>
-</header>
-<main>
-    <a href='./back/add_vote.php'>新增投票</a>    
-    <a href='./back/edit_vote.php'>編輯投票</a>    
-    <a href='./back/del_vote.php'>刪除投票</a>    
-    <a href='./back/query_vote.php'>結果查詢</a>   
-      
-</main>
-<footer></footer>
+    <header>
+        <a href="index.php">首頁</a>
+        <a href="login.php">登出</a>
+    </header>
+    <main>
+        <nav>
+            <a href='./back/add_vote.php'>新增投票</a>
+            <a href='./back/edit_vote.php'>編輯投票</a>
+            <a href='./back/del_vote.php'>刪除投票</a>
+            <a href='./back/query_vote.php'>結果查詢</a>
+        </nav>
+        <ul class="topic-list">
+            <li class="list-row">
+                <div class="list-item-title">主題</div>
+                <div class="list-item-title">狀態</div>
+                <div class="list-item-title">期間</div>
+                <div class="list-item-title">投票數</div>
+                <div class="list-item-title">操作</div>
+            </li>
+            <?php
+            $sql = "select * from `topics`";
+            $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($rows as $row) {
+            ?>
+                <ul class="topic-list">
+                    <li class="list-row">
+                        <div class="list-item-title"><?= $row['subject'] ?>
+                        </div>
+                        <div class="list-item-title"></div>
+                        <div class="list-item-title"><?= $row['open_time'] . '~' . $row['close_time'] ?></div>
+                        <div class="list-item-title"></div>
+                        <div class="list-item-title"></div>
+                        <div class="list-time"></div>
+                        <div class="list-time">
+                            <button onclick="location.href='./back/edit_vote.php?id=<?=$row['id'];?>'">編輯</button>
+                            <button onclick="location.href='./back/del_vote.php?id=<?=$row['id'];?>'">刪除</button>
+                        </div>
+                    </li>
+                <?php
+            }
+                ?>
+    </main>
+    <footer></footer>
 </body>
+
 </html>

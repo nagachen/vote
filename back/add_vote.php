740039f7678840adfa738b2cdaa13e424465387e
nagachen <birdann99@gmail.com>
2023-05-19 18:56:31 +0800

    建立新增主題表單

---
+++ back/add_vote.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新增主題</title>
+</head>
+<body>
+    <div>
+    <h1>新增主題</h1>
+    <input type="text" name="subject" id="subject">
+</div>
+<div>
+    <input type="submit" value="新增">
+
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>驚奇投票所</title>
+    <title>不合格投票所</title>
 </head>
 <body>
 <header>

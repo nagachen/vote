61e57a3824507214cd625c7621307b64769a6e03
nagachen <birdann99@gmail.com>
2023-05-19 18:36:16 +0800

    建立基礎架構所需的頁面及整理專案資料夾結構

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>驚奇投票所</title>
+</head>
+<body>
+<header>
+    <a href="index.php">首頁</a>
+    <a href="result.php">結果</a>
+    <a href="login.php">登入</a>
+    <a href="reg.php">註冊</a>
+</header>
+<main></main>
+<footer></footer>
+</body>
+</html>
